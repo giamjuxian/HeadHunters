868d08cc6216cb850773a2321f016ca51005dabe
giamjuxian <giamjuxian@gmail.com>
2018-11-17 16:42:37 +0800

    Add Ui prefab

---
+++ Assets/Jeff/ButtonInitialise.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInitialise : MonoBehaviour
+{
+    // Function for buttons
+    public void buttonTestLog()
+    {
+        Debug.Log("Welcome to AFRA!");
+    }
+}
---
+++ Assets/Jeff/ButtonTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+
+    // Function for buttons
+    public void buttonTestLog() {
+        Debug.Log("Hello World");
+    }
+}
