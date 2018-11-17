1bdc9efa8d172ed2ae29ea24f3ffcf4e2c337150
Giam Ju Xian <giamjuxian@gmail.com>
2018-11-17 17:38:18 +0800

    Add control for washing

---
+++ Assets/Script/EffectManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour {
+    [SerializeField]
+    GameObject soapBubblesEffect;
+    [SerializeField]
+    GameObject waterWashingEffect;
+    [SerializeField]
+    GameObject dandruffParticle;
+
+    float currCountdownValue;
+
+    public void playHairWash() {
+        ParticleSystem soapBubblesParticle = soapBubblesEffect.GetComponent<ParticleSystem>();
+        ParticleSystem waterWashingParticle = waterWashingEffect.GetComponent<ParticleSystem>();
+
+        soapBubblesParticle.Play();
+        waterWashingParticle.Play();
+        StartCoroutine(HideDandruffParticle());
+
+    }
+
+
+    public IEnumerator HideDandruffParticle(float countdownValue = 5)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+        dandruffParticle.SetActive(false);
+    }
+}
